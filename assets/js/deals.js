8c390ed7f8bc52fe94eb47d13608218fb31ea046
PallaviG <pallavigupto@gmail.com>
2019-09-12 21:38:23 +0530

    updated deals.js

--- assets/js/deals.js
+++ assets/js/deals.js
@@ -1,15 +1,7 @@
 var ref = document.referrer;
-console.log("booo", ref)
-if (ref.includes("sustainablesites"))  
+if (ref.includes("reviewindex"))  
 {
-   alert("referrer if",ref);
    var current_url = window.location.href;
    window.location = current_url.substring(51,);
 }
-else
-{
-   alert("referrer else:",ref);
- // window.location = 'https://www.amazon.in/';
-}
-
 
