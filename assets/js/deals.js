f60c7969aa7f9c909386a37a9cb21aa964eb2ac3
PallaviG <pallavigupto@gmail.com>
2019-09-15 16:29:25 +0530

    fixed load-time

--- assets/js/deals.js
+++ assets/js/deals.js
@@ -1,7 +1,12 @@
 var ref = document.referrer;
+var current_url = window.location.href;
+var destination_url = current_url.substring(51,);
 if (ref.includes("reviewindex"))  
 {
-   var current_url = window.location.href;
-   window.location = current_url.substring(51,);
+  $(document).ready(function() {
+  document.getElementsByTagName("deals-html")[0].style.visibility = "visible";
+  });
+  window.location = destination_url;
 }
+
 
