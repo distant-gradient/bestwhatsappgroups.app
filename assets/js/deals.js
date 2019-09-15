68ed1f85413bf41e0147942f57403e20d8f80e61
PallaviG <pallavigupto@gmail.com>
2019-09-15 16:48:48 +0530

    updates

--- assets/js/deals.js
+++ assets/js/deals.js
@@ -3,9 +3,6 @@ var current_url = window.location.href;
 var destination_url = current_url.substring(51,);
 if (ref.includes("reviewindex"))  
 {
-  $(document).ready(function() {
-  document.getElementsByTagName("deals-html")[0].style.visibility = "visible";
-  });
   window.location = destination_url;
 }
 
