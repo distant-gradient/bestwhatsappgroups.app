c2e427c9057e470babcda70d4419a3a404d58c01
Dyuti <root@thereviewindex.com>
2019-09-18 20:07:52 +0530

    checkpoint - need to move hide/show logic to head before rendering anything AND only for certain post

--- assets/js/deals.js
+++ assets/js/deals.js
@@ -1,11 +1,15 @@
 var ref = document.referrer;
 var current_url = window.location.href;
-var destination_url = current_url.substring(51,);
-if (ref.includes("reviewindex"))  
+var new_url = current_url.split('?url=')[1]
+if (ref.indexOf("sustainablesites") != -1)  
 {
-  //window.location = destination_url;
-  console.log(destination_url)
+  document.getElementById('post_post').style.display = 'none'; 
+  document.getElementById('post_loader').style.display = 'block'; 
+  window.location = new_url
+}
+else
+{
+  document.getElementById('post_post').style.display = 'block'; 
+  document.getElementById('post_loader').style.display = 'none'; 
 }
 
-else
-  console.log(ref)
